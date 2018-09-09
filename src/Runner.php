4328cc7f1a049dbed9e4d02fcb4fa535118c0b4c
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-09 14:41:30 +0200

    add run method

--- src/Runner.php
+++ src/Runner.php
@@ -6,5 +6,15 @@ class Runner
 {
     const ERROR_NO_RECIPE = 1;
     const ERROR_RECIPE_NOT_FOUND = 2;
+
+    public function __construct(Recipe $recipe)
+    {
+        $this->recipe = $recipe;
+    }
+
+    public function run() : void
+    {
+        $this->recipe->process();
+    }
 }
 
