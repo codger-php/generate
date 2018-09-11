5a859380a105a3f9305dd7b569180ff45441a9a9
Marijn Ophorst <marijn@monomelodies.nl>
2018-09-11 07:02:26 +0200

    add better error handling and allow optoinal custom path

--- src/Runner.php
+++ src/Runner.php
@@ -2,25 +2,51 @@
 
 namespace Codger\Generate;
 
+use ReflectionFunction;
+
 class Runner
 {
     const ERROR_NO_RECIPE = 1;
     const ERROR_RECIPE_NOT_FOUND = 2;
 
+    private $recipe;
     private $path;
 
-    public function __construct(string $path)
+    public function __construct(string $recipe, string $path = null)
     {
+        $this->recipe = $recipe;
+        if (!$path) {
+            $path = getcwd();
+        }
         $this->path = $path;
     }
 
     public function run(string ...$argv) : void
     {
-        if (file_exists($this->path)) {
-            $recipe = require $this->path;
-            call_user_func($recipe, ...$argv)->process();
+        if (file_exists("{$this->path}/recipes/{$this->recipe}/Recipe.php")) {
+            $recipe = require "{$this->path}/recipes/{$this->recipe}/Recipe.php";
+            $reflection = new ReflectionFunction($recipe);
+            if ($reflection->getNumberOfRequiredParameters() > count($argv)) {
+                $wanteds = $reflection->getParameters();
+                $usage = call_user_func(function (array &$wanteds) : string {
+                    $param = array_shift($wanteds);
+                    $out = '';
+                    if ($param->isOptional()) {
+                        $out .= ' [';
+                    }
+                    $out .= $param->getName();
+                    if ($wanteds) {
+                        $out .= call_user_func(__FUNCTION__, $wanteds);
+                    }
+                    $out .= ']';
+                    return $out;
+                });
+                fwrite(STDERR, "Usage: `$ vendir/bin/codger {$this->recipe} $usage\n");
+            } else {
+                call_user_func($recipe, ...$argv)->process();
+            }
         } else {
-            fwrite(STDERR, "Recipe {$this->path} could not be found, skipping...\n");
+            fwrite(STDERR, "Recipe `{$this->recipe}` could not be found in `{$this->path}/recipes`, skipping...\n");
         }
     }
 
