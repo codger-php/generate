2ca89be4e3d710af2ec0598afbeb491c56a86318
Marijn Ophorst <marijn@sensimedia.nl>
2018-09-21 11:51:24 +0200

    add docblocks, output buffering and actually use $format during reading

--- src/FakeInOut.php
+++ src/FakeInOut.php
@@ -2,30 +2,77 @@
 
 namespace Codger\Generate;
 
+/**
+ * Fake input/ouput handler.
+ */
 class FakeInOut implements InOut
 {
+    /** @var array */
     private $stack = [];
+    /** @var string */
+    private $output = '';
 
+    /**
+     * Fake input reader. Shifts elements off the stack.
+     *
+     * @param string|null $format Optional format
+     * @return string
+     */
     public function read(string $format = null) : string
     {
         $input = array_shift($this->stack);
+        if (isset($format)) {
+            $input = sprintf($format, $input);
+        }
         return $input ?? '';
     }
 
+    /**
+     * Dummy writer.
+     *
+     * @param string $output
+     * @return void
+     * @see Codger\Generate\FakeInOut::flush
+     */
     public function write(string $output) : void
     {
-        // noop
+        $this->output .= $output;
     }
 
-    public function error(string $output) : void
+    /**
+     * Dummy error handler.
+     *
+     * @param string $error
+     * @return void
+     */
+    public function error(string $error) : void
     {
-        // noop
+        $this->write($error);
     }
 
+    /**
+     * Add an expectation, i.e. input prompted by e.g.
+     * `Codger\Generate\Recipe::answer`.
+     *
+     * @param string $expectation Expected input
+     * @return Codger\Generate\FaceInOut Itself, for chaining.
+     */
     public function expect(string $expectation) : FakeInOut
     {
         $this->stack[] = $expectation;
         return $this;
+    }
+
+    /**
+     * Returns the output so far, and resets the cache.
+     *
+     * @return string
+     */
+    public function flush() : string
+    {
+        $output = $this->output;
+        $this->output = '';
+        return $output;
     }
 }
 
