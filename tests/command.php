c790edbe1cf8920ae0bc073201ab897a0cd2e2d8
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-20 16:25:31 +0100

    start fixing tests

--- tests/command.php
+++ tests/command.php
@@ -5,11 +5,11 @@ Codger\Generate\Recipe::setInOut($inout);
 
 /** Testsuite for Codger\Generate\Command */
 return function () use ($inout) : Generator {
-    $runner = new Codger\Generate\Command;
+    $runner = new Codger\Generate\Command([]);
 
     /** run yields $result === null */
     yield function () use ($runner) {
-        $result = $runner('demo:chef', 'Chocolate cake');
+        $result = $runner('demo:chef', "'Chocolate cake'");
         assert($result === null);
     };
 };
