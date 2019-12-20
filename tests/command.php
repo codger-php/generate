b8ee07e4a875be242d95a10b6ddd975b86cbba74
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-20 16:08:40 +0100

    this can now be greatly simplified, we have less to test

--- tests/command.php
+++ tests/command.php
@@ -5,51 +5,12 @@ Codger\Generate\Recipe::setInOut($inout);
 
 /** Testsuite for Codger\Generate\Command */
 return function () use ($inout) : Generator {
-    $runner = new Codger\Generate\Command('chef');
-
-    /** Arguments method strips the -w flag */
-    yield function () use ($runner) {
-        $GLOBALS['argv'] = ['', '', '-w'];
-        $result = $runner->arguments([]);
-        assert($result === []);
-        putenv("CODGER_DRY=1");
-        $GLOBALS['argv'] = ['', ''];
-    };
+    $runner = new Codger\Generate\Command;
 
     /** run yields $result === null */
     yield function () use ($runner) {
-        $result = $runner->run('Chocolate cake');
+        $result = $runner('demo:chef', 'Chocolate cake');
         assert($result === null);
-    };
-
-    /** hasOption returns true even if the option was negated */
-    yield function () use ($runner) {
-        $runner->setOptions(['^blarps']);
-        $result = $runner->hasOption('blarps');
-        assert($result === true);
-    };
-
-    /** askedFor returns false if the option was negated */
-    yield function () use ($runner) {
-        $result = $runner->askedFor('blarps');
-        assert($result === false);
-    };
-
-    /** an option with a default appears set, even if not specified explicitly */
-    yield function () use ($runner) {
-        $runner->setOptions([]);
-        $runner->defaults('blarps');
-        $result = $runner->hasOption('blarps');
-        assert($result === true);
-    };
-
-    /** we can set aliases */
-    yield function () use ($inout) {
-        $runner = new Codger\Generate\Command('chefspecial', (object)[
-            'aliases' => (object)['chefspecial' => ['chef', 'Chocolate mousse']]
-        ]);
-        $runner->run();
-        assert(strpos($inout->flush(), 'Chocolate mousse') !== false);
     };
 };
 
