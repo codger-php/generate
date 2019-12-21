6b2238cd131c30caa2c57752b72458ad8e87f544
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 05:51:44 +0100

    use a defaultoptions trait for this

--- src/Command.php
+++ src/Command.php
@@ -8,17 +8,12 @@ use stdClass;
 
 class Command extends Cliff\Command
 {
+    use DefaultOptions;
+
     /** @var int */
     const ERROR_NO_RECIPE = 1;
     /** @var int */
     const ERROR_RECIPE_NOT_FOUND = 2;
-
-    /**
-     * The base path where output is written to, relative to CWD.
-     *
-     * @var string|null
-     */
-    public $output = null;
 
     public function __invoke(string $recipe)
     {
