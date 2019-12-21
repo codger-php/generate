f132f0115e6dec2256867c023ed1566219571394
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-21 08:12:16 +0100

    fix tests

--- tests/command.php
+++ tests/command.php
@@ -5,12 +5,23 @@ Codger\Generate\Recipe::setInOut($inout);
 
 /** Testsuite for Codger\Generate\Command */
 return function () use ($inout) : Generator {
-    $runner = new Codger\Generate\Command([]);
 
-    /** run yields $result === null */
-    yield function () use ($runner) {
-        $result = $runner('demo:chef', "'Chocolate cake'");
-        assert($result === null);
+    /** We can execute the demo command and get a chocolate cake. */
+    yield function () use ($inout) {
+        $runner = new Codger\Generate\Command(['demo:chef', "Chocolate cake"]);
+        $runner->execute();
+        $result = $inout->flush();
+        assert(strpos($result, 'Chocolate cake') !== false);
+        assert(strpos($result, 'Chocolate sauce') === false);
+    };
+
+    /** We can specifically request for sauce. */
+    yield function () use ($inout) {
+        $runner = new Codger\Generate\Command(['demo:chef', "Chocolate cake", '--sauce']);
+        $runner->execute();
+        $result = $inout->flush();
+        assert(strpos($result, 'Chocolate cake') !== false);
+        assert(strpos($result, 'Chocolate sauce') !== false);
     };
 };
 
