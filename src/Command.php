6bf8a3bb3e6e090c8ac2264a7ddf22fdc3812e07
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-23 12:21:30 +0100

    super-simplify: most of the heavy lifting is done by cliff now

--- src/Command.php
+++ src/Command.php
@@ -6,6 +6,12 @@ use Monolyth\Cliff;
 use ReflectionFunction;
 use stdClass;
 
+/**
+ * Generate code according to the supplied recipe.
+ *
+ * Usage:
+ * $ vendor/bin/codger [OPTIONS] recipe
+ */
 class Command extends Cliff\Command
 {
     use DefaultOptions;
@@ -22,31 +28,33 @@ class Command extends Cliff\Command
     /** @var array|null */
     private $_arguments;
 
-    public function __construct(array $arguments = null)
+    /**
+     * Constructor.
+     *
+     * @param array|null $arguments
+     * @param Monolyth\Cliff\Command|null $forwardingCommand
+     * @return void
+     */
+    public function __construct(array $arguments = null, Cliff\Command $forwardingCommand = null)
     {
-        parent::__construct($arguments, false);
+        parent::__construct($arguments, $forwardingCommand);
         $this->_arguments = $arguments;
     }
 
-    public function __invoke(string $recipe)
+    /**
+     * Supply at least the name of the recipe. Note that all recipes _must_ be
+     * namespaced in the `\Codger\` namespace. This is so you can autoload them
+     * outside your regular codebase. You should omit the `codger` prefix on the
+     * CLI.
+     *
+     * Per `monolyth/cliff` convention, you can use `:` or `/` as a namespace
+     * separator on the CLI. Use `snake-case` for `snakeCase` syntax.
+     *
+     * @param string $recipe
+     * @return void
+     */
+    public function __invoke(string $recipe) : void
     {
-        if (isset($this->_arguments)) {
-            $argv = $this->_arguments;
-        } else {
-            $argv = $_SERVER['argv'];
-            array_shift($argv); // The executable. Ignore.
-        }
-        array_shift($argv); // This was the recipe name; no longer needed.
-        $recipeClass = Recipe::toClassName($recipe);
-        if (!class_exists($recipeClass)) {
-            throw new RecipeNotFoundException("The recipe `$recipeClass` could not be autoloaded; does it exist?", self::ERROR_RECIPE_NOT_FOUND);
-        }
-        $recipe = new $recipeClass($argv);
-        if (!($recipe instanceof Recipe)) {
-            throw new RecipeIsNotARecipeException("The recipe `$recipeClass` does not extend `Codger\\Generate\\Recipe`; aborting.", self::ERROR_RECIPE_IS_NOT_A_RECIPE);
-        }
-        $recipe->execute();
-        $recipe->process();
     }
 }
 
