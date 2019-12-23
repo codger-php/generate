ee60754044431c3b7bd0bdcf3946c5ff348cc619
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-23 12:21:54 +0100

    fix command names in test

--- tests/command.php
+++ tests/command.php
@@ -8,7 +8,7 @@ return function () use ($inout) : Generator {
 
     /** We can execute the demo command and get a chocolate cake. */
     yield function () use ($inout) {
-        $runner = new Codger\Generate\Command(['demo:chef', "Chocolate cake"]);
+        $runner = new Codger\Generate\Command(['codger:demo:chef', "Chocolate cake"]);
         $runner->execute();
         $result = $inout->flush();
         assert(strpos($result, 'Chocolate cake') !== false);
@@ -17,7 +17,7 @@ return function () use ($inout) : Generator {
 
     /** We can specifically request for sauce. */
     yield function () use ($inout) {
-        $runner = new Codger\Generate\Command(['demo:chef', "Chocolate cake", '--sauce']);
+        $runner = new Codger\Generate\Command(['codger:demo:chef', "Chocolate cake", '--sauce']);
         $runner->execute();
         $result = $inout->flush();
         assert(strpos($result, 'Chocolate cake') !== false);
