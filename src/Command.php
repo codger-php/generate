db5e07970ee39247f407d915696be9770b07fc76
Marijn Ophorst <marijn@monomelodies.nl>
2021-01-10 18:56:04 +0100

    forwarding is deprecated/removed, so re-implement the invoke method

--- src/Command.php
+++ src/Command.php
@@ -53,6 +53,23 @@ class Command extends Cliff\Command
      */
     public function __invoke(string $recipe) : void
     {
+        if (isset($this->_arguments)) {
+            $argv = $this->_arguments;
+        } else {
+            $argv = $_SERVER['argv'];
+            array_shift($argv); // The executable. Ignore.
+        }
+        array_shift($argv); // This was the recipe name; no longer needed.
+        $recipeClass = Recipe::toClassName($recipe);
+        if (!class_exists($recipeClass)) {
+            throw new RecipeNotFoundException("The recipe `$recipeClass` could not be autoloaded; does it exist?", self::ERROR_RECIPE_NOT_FOUND);
+        }
+        $recipe = new $recipeClass($argv);
+        if (!($recipe instanceof Recipe)) {
+            throw new RecipeIsNotARecipeException("The recipe `$recipeClass` does not extend `Codger\\Generate\\Recipe`; aborting.", self::ERROR_RECIPE_IS_NOT_A_RECIPE);
+        }
+        $recipe->execute();
+        $recipe->process();
     }
 }
 
