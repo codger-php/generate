e986c858369a86bb54b58e3634b8c18d6a3ddfb1
Marijn Ophorst <marijn@monomelodies.nl>
2022-11-24 15:44:16 +0100

    make more php8-ey

--- src/Command.php
+++ src/Command.php
@@ -15,28 +15,20 @@ class Command extends Cliff\Command
 {
     use DefaultOptions;
 
-    /** @var int */
     const ERROR_NO_RECIPE = 2;
-    /** @var int */
     const ERROR_RECIPE_NOT_FOUND = 3;
-    /** @var int */
     const ERROR_RECIPE_IS_NOT_A_RECIPE = 4;
-    /** @var int */
     const ERROR_TWIG_ENVIRONMENT_NOT_SET = 5;
-
-    /** @var array|null */
-    private ?array $_arguments;
 
     /**
      * Constructor.
      *
-     * @param array|null $arguments
+     * @param array|null $_arguments
      * @return void
      */
-    public function __construct(array $arguments = null)
+    public function __construct(private ?array $_arguments = null)
     {
-        parent::__construct($arguments);
-        $this->_arguments = $arguments;
+        parent::__construct($_arguments);
     }
 
     /**
